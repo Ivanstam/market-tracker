d4e969b5b6059125e4000d084ad06bde9dcbfd0e
Ivan Stam <ivanstam18@gmail.com>
2023-06-05 14:58:30 +0200

    Router authentication layout and path
    Split authentication components into their views/components and added a route parent for them to simplify the authentication logic
    Simple readme

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,32 +4,53 @@ import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import Stocks from "../views/Stocks.vue";
 import DefaultLayout from "../components/DefaultLayout.vue"
+import Authlayout from "../components/Authlayout.vue"
+import store from "../store/index.js";
 
 const routes = [
   {
     path: '/',
     redirect: '/dashboard',
     component: DefaultLayout,
+    meta: {requiresAuth: true},
     children: [
       {path: '/dashboard', name: 'Dashboard', component: Dashboard},
       {path: '/stocks', name: 'Stocks', component: Stocks}
     ]
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: '/auth',
+    name: 'Auth',
+    redirect: '/login',
+    component: Authlayout,
+    children: [{
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    }]
   },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  }
+
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+// Redirect to login/register or the dashboard when unauthorized or authorized respectively
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({name: 'Login'});
+  } else if (store.state.user.token && (to.name === 'Login' || to.name === 'Register')) {
+    next({name: 'Dashboard'});
+  } else {
+    next();
+  }
 });
 
 export default router;
